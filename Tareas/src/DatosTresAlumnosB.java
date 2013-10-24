63d821d6af876dc266d3d4c170fe9dfb14c1476b
K <K@K-PC>
2013-10-24 16:25:51 +0200

    up

--- Tareas/src/DatosTresAlumnosB.java
+++ Tareas/src/DatosTresAlumnosB.java
@@ -1,40 +1,64 @@
 import java.util.Scanner;
 
-public class DatosTresAlumnos {
+public class DatosTresAlumnosB {
 
 	public static void main(String[] args) {
 		/*
 		 * Utilizando como base los ejemplos vistos en clase, haz un programa que pida los datos de tres alumnos, 
 		 * de los que nos interesa conocer su nombre y edad. Sabemos que los alumnos tienen la misma edad, 
 		 * con lo que pediremos la edad una única vez. Para finalizar, el programa visualizará los datos de los tres alumnos.
+		 * 
+		 * Modifica el programa anterior de forma que la edad de los alumnos ya no sea la misma. 
+		 * El programa debe calcular y mostrar la edad media de los tres alumnos.
 		 */
 		
 		//Variables
 		
-		 int edadAlumnos;
+		 int edadAlumno1, edadAlumno2, edadAlumno3;
 		 
 		 Scanner input = new Scanner(System.in);
 		 
 		 //Inserción de datos (edad y años)
 		 
+		 	//Al1
+		 
 			System.out.print("Introduce el nombre del primer alumno ");
 			String nombreAlumno1 = input.next();
+			
+			System.out.print("Introduce la edad del primer alumno ");
+			edadAlumno1 = input.nextInt();
+			
+			//Al2
 			
 			System.out.print("Introduce el nombre del segundo alumno ");
 			String nombreAlumno2 = input.next();
 			
+			System.out.print("Introduce la edad del segundo alumno ");
+			edadAlumno2 = input.nextInt();
+			
+			 //Al3
+			
 			System.out.print("Introduce el nombre del tercer alumno ");
 			String nombreAlumno3 = input.next();
 			
-			System.out.print("Introduce la edad de los alumnos ");
-			edadAlumnos = input.nextInt();
+			System.out.print("Introduce la edad del tercer alumno ");
+			edadAlumno3 = input.nextInt();
+		
 			
 		//Visulacización de datos	
+			
+			System.out.print("\nNombres de alumnos ");
 			
 			System.out.println("\nEl nombre del primer alumno es " + nombreAlumno1);
 			System.out.println("El nombre del segundo alumno es " + nombreAlumno2);
 			System.out.println("El nombre del tercer alumno es " + nombreAlumno3);
-			System.out.print("\nLa edad de los alumnos es de " + edadAlumnos + " años");
+			
+			System.out.println("\nEdades de alumnos ");
+			
+			System.out.println("\nLa edad de los alumnos es de " + edadAlumno1 + " años");
+			System.out.println("La edad de los alumnos es de " + edadAlumno2 + " años");
+			System.out.println("La edad de los alumnos es de " + edadAlumno3 + " años");
+			
 
 	}
 
---
+++ Tareas/src/Operaciones/Aritmeticas/Ejercicios.java
@@ -0,0 +1,56 @@
+package Operaciones.Aritmeticas;
+
+public class Ejercicios {
+
+	public static void main(String[] args) {
+	
+		
+		/*
+		 *1- (-5^2+10)*3
+		 *2 35,3 - 53/10
+		 *3 1+5%1.5+8/2
+		 *4 -(-2^5+18/5)
+		 * 65/4/4.0*3
+		 * 44%10+7-25^10-2
+		 * 4.0/10.0+3.5*2
+		 * 10%4+6/2
+		 * (|4-20/3|)^3
+		 * raizcuadrada(4.5-5-0)+7*3
+		 * 3*10/3+10%3
+		 * 3L^3
+		 * (36,0/4)+(5*3)
+		 * (28/8)*2
+		 * (13*3)%10
+		 * 2^3^3
+		 * 
+		 */
+		
+		
+System.out.println("El resultado es "+ (((Math.pow(-5, 2))+10)*3));
+System.out.println("El resultado es "+ (35.3-53)/10);
+System.out.println("El resultado es "+ (1+((5%1.5)*8)/2));
+System.out.println("El resultado es "+ -(Math.pow(-2, 5)+(18/5)));
+System.out.println("El resultado es "+ 65/3/4.0*3);
+System.out.println("El resultado es "+ (44%10)+(7+(Math.pow(-25, 10)-2)));
+System.out.println("El resultado es "+ (4.0/10.0)+3.5*2);
+System.out.println("El resultado es "+ 10%4 + (6/2));
+System.out.println("El resultado es "+ Math.pow(Math.abs(4-(20/3)), 3));
+System.out.println("El resultado es "+ Math.sqrt((4.5-5.0)+7*3));
+System.out.println("El resultado es "+ (3*10/3)+10%3);
+System.out.println("El resultado es "+ (Math.pow(3, 3)));
+
+System.out.println("El resultado es "+ (Math.pow(2, (Math.pow(3, 3)))));
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
